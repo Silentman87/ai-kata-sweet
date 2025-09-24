6ff16f1cdcc0396f451b6c5511dda3042710bdac
shravan <choudharyshravan8965@gmail.com>
2025-09-25 01:01:07 +0530

    add controller and route for login and register

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-//route not written yet
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use('/api/auth', authRoutes);
 
 
 // MongoDB Connection
